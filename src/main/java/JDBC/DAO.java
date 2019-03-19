528e54b8274c953f8549454ae84cee34096d085a
Abdo Alexandre <alexandre.abdo@etud.univ-jfc.fr>
2019-03-19 15:23:29 +0100

    Implémentation de la méthode "getCustomerByName"

--- src/main/java/JDBC/DAO.java
+++ src/main/java/JDBC/DAO.java
@@ -5,6 +5,15 @@
  */
 package JDBC;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.sql.DataSource;
 
 /**
@@ -12,7 +21,7 @@ import javax.sql.DataSource;
  * @author Aabdo
  */
 public class DAO {
-    
+
     protected final DataSource myDataSource;
 
     /**
@@ -20,6 +29,58 @@ public class DAO {
      * @param dataSource la source de données à utiliser
      */
     public DAO(DataSource dataSource) {
-	this.myDataSource = dataSource;
+        this.myDataSource = dataSource;
+    }
+
+    /**
+     *
+     * @return le nombre d'enregistrements dans la table CUSTOMER
+     * @throws DAOException
+     */
+    public int numberOfCustomers() throws DAOException {
+        int result = 0;
+
+        String sql = "SELECT COUNT(*) AS NUMBER FROM CUSTOMER";
+        // Syntaxe "try with resources" 
+        // cf. https://stackoverflow.com/questions/22671697/try-try-with-resources-and-connection-statement-and-resultset-closing
+        try (Connection connection = myDataSource.getConnection(); // Ouvrir une connexion
+                Statement stmt = connection.createStatement(); // On crée un statement pour exécuter une requête
+                ResultSet rs = stmt.executeQuery(sql) // Un ResultSet pour parcourir les enregistrements du résultat
+                ) {
+            if (rs.next()) { // Pas la peine de faire while, il y a 1 seul enregistrement
+                // On récupère le champ NUMBER de l'enregistrement courant
+                result = rs.getInt("NUMBER");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger("DAO").log(Level.SEVERE, null, ex);
+            throw new DAOException(ex.getMessage());
+        }
+
+        return result;
+    }
+    
+    public List<CustomerEntity> getCustomerByName(String name) throws DAOException{
+        List<CustomerEntity> res = new ArrayList();
+        String sql = "SELECT * FROM CUSTOMER WHERE NAME = ?";
+        try(Connection connexion = myDataSource.getConnection();
+            PreparedStatement stmt = connexion.prepareStatement(sql);){
+            stmt.setString(1,name);
+            try(ResultSet r = stmt.executeQuery()){
+                while (r.next()){ // Tant qu'il y a des enregistrements
+                    // On récupère les champs nécessaires de l'enregistrement courant
+                    int id = r.getInt("CUSTOMER_ID");
+                    String address = r.getString("ADDRESSLINE1");
+                    // On crée l'objet entité
+                    CustomerEntity c = new CustomerEntity(id, name, address);
+                    // On l'ajoute à la liste des résultats
+                    res.add(c);
+                }
+            }
+        }
+        catch(SQLException ex){
+            Logger.getLogger("DAO").log(Level.SEVERE, null, ex);
+            throw new DAOException(ex.getMessage());
+        }
+        return res;
     }
 }
--- src/test/java/JDBC/DAOTest.java
+++ src/test/java/JDBC/DAOTest.java
@@ -5,6 +5,7 @@
  */
 package JDBC;
 
+import static JDBC.DataSourceFactory.getDataSource;
 import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
@@ -18,62 +19,71 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.Ignore;
+import org.hsqldb.jdbc.JDBCDataSource;
 
 /**
  *
  * @author Utilisateur
  */
 public class DAOTest {
-    private DAO myDAO; // L'objet à tester
-    private DataSource myDataSource; // La source de données à utiliser
-    private static Connection myConnection ; // La connection à la BD de test
+	private DAO myDAO; // L'objet à tester
+	private DataSource myDataSource; // La source de données à utiliser
+	private static Connection myConnection ; // La connection à la BD de test
 	
 
-    @Before
-    public void setUp() throws SQLException, IOException, SqlToolError {
-    	// On utilise la base de données de test
-    	myDataSource = getTestDataSource();
-    	myConnection = myDataSource.getConnection();
-    	// On crée le schema de la base de test
-	executeSQLScript(myConnection, "schema.sql");
-	// On y met des données
-	executeSQLScript(myConnection, "bigtestdata.sql");		
-	
-	myDAO = new DAO(myDataSource);
-    }
-    
-    @Before
-    public static void setUpClass() {
-    }
-    
-    @After
-    public static void tearDownClass() {
-    }
-    
-    private void executeSQLScript(Connection connexion, String filename)  throws IOException, SqlToolError, SQLException {
-	// On initialise la base avec le contenu d'un fichier de test
-	String sqlFilePath = DAOTest.class.getResource(filename).getFile();
-	SqlFile sqlFile = new SqlFile(new File(sqlFilePath));
-	sqlFile.setConnection(connexion);
-	sqlFile.execute();
-	sqlFile.closeReader();		
-    }
-    public static DataSource getTestDataSource() {
-	org.hsqldb.jdbc.JDBCDataSource ds = new org.hsqldb.jdbc.JDBCDataSource();
-	ds.setDatabase("jdbc:hsqldb:mem:testcase;shutdown=true");
-	ds.setUser("sa");
-	ds.setPassword("sa");
-	return ds;
-    }
-    
-    @After
-    public void tearDown() {
-    }
+	@Before
+	public void setUp() throws SQLException, IOException, SqlToolError {
+		// On utilise la base de données de test
+		myDataSource = getDataSource();
+		myConnection = myDataSource.getConnection();
+		// On crée le schema de la base de test
+		//executeSQLScript(myConnection, "schema.sql");
+		// On y met des données
+		//executeSQLScript(myConnection, "bigtestdata.sql");	
+		
+		myDAO = new DAO(myDataSource);
+	}
 
-    @Test
-    public void testSomeMethod() {
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+	@After
+	public void tearDown() throws IOException, SqlToolError, SQLException {
+		myConnection.close(); // La base de données de test est détruite ici
+             	myDAO = null; // Pas vraiment utile
+	}
+	
+        
+	public static DataSource getTestDataSource() {
+		org.hsqldb.jdbc.JDBCDataSource ds = new org.hsqldb.jdbc.JDBCDataSource();
+		ds.setDatabase("jdbc:hsqldb:mem:testcase;shutdown=true");
+		ds.setUser("sa");
+		ds.setPassword("sa");
+		return ds;
+	}
+        
+	private void executeSQLScript(Connection connexion, String filename)  throws IOException, SqlToolError, SQLException {
+		// On initialise la base avec le contenu d'un fichier de test
+		String sqlFilePath = DAOTest.class.getResource(filename).getFile();
+		SqlFile sqlFile = new SqlFile(new File(sqlFilePath));
+
+		sqlFile.setConnection(connexion);
+		sqlFile.execute();
+		sqlFile.closeReader();		
+	}
+	
+	/**
+	 * Test of numberOfCustomers method, of class DAO.
+	 * @throws simplejdbc.DAOException
+	 */
+	@Test
+	public void testNumberOfCustomers() throws DAOException {
+		int result = myDAO.numberOfCustomers();
+		assertEquals(13, result);
+	}
+        
+        @Test
+        public void testGetCustomerByName() throws DAOException{
+            int id = myDAO.getCustomerByName("Jumbo Eagle Corp").get(0).getCustomerId();
+            assertEquals(1,id);
+            assertEquals(0,myDAO.getCustomerByName("a").size());
+        }
     
 }
